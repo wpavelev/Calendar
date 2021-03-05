5b3a1ba03cfedb07efb074de0c90a988eac8aa47
wpavelev <wpa@posteo.de>
2021-03-05 19:25:30 +0100

    init

--- app/src/main/java/de/wpavelev/calendar/CalendarUtil.java
+++ app/src/main/java/de/wpavelev/calendar/CalendarUtil.java
@@ -22,8 +22,7 @@ public class CalendarUtil {
         mContext = context;
     }
 
-    // Projection array. Creating indices for this array instead of doing
-    // dynamic lookups improves performance.
+
     public static final String[] CAL_PROJECTION = new String[]{
             CalendarContract.Calendars._ID,                           // 0
             CalendarContract.Calendars.ACCOUNT_NAME,                  // 1
@@ -31,7 +30,7 @@ public class CalendarUtil {
             CalendarContract.Calendars.OWNER_ACCOUNT                  // 3
     };
 
-    // The indices for the projection array above.
+
     private static final int PROJECTION_ID_INDEX = 0;
     private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
     private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
@@ -46,92 +45,23 @@ public class CalendarUtil {
     };
 
 
-    public void debugListCal() {
-        Log.d(TAG, "debugListCal: ");
-        for (CalInfo calInfo : getAllCalendars()) {
-            Log.d(TAG, "ID: " + calInfo.getId() + " Name: " + calInfo.getName());
-        }
-    }
-
-    public void debugListEvents() {
-        Log.d(TAG, "debugListEvents: ");
-        for (EventInfo eventInfo : getEventList(10)) {
-            Calendar begin = Calendar.getInstance();
-            Calendar end = Calendar.getInstance();
-            begin.setTimeInMillis(eventInfo.start);
-            end.setTimeInMillis(eventInfo.end);
-            Log.d(TAG, "event " + eventInfo.getId() + ": " + eventInfo.getTitle() + ":" +
-                    convertCalToString(begin) + " - " + convertCalToString(end));
-
-        }
-    }
-
-    public void convertEmptyTitles() {
-        for (EventInfo eventInfo : getEventList(10)) {
-            Calendar begin = Calendar.getInstance();
-            Calendar end = Calendar.getInstance();
-            begin.setTimeInMillis(eventInfo.start);
-            end.setTimeInMillis(eventInfo.end);
-            updateEventTitle(eventInfo.getId(), getTitleFromTime(begin, end));
-
-        }
-    }
-
-
-    public void emptyTitles() {
-        Log.d(TAG, "emptyTitles:");
-        for (EventInfo eventInfo : getEventList(10)) {
-
-            if (eventInfo.getTitle().contains("Urlaub")) {
-                continue;
-            }
-            updateEventTitle(eventInfo.getId(), "");
-
-        }
-    }
-
-
-    private String convertCalToString(Calendar calendar) {
-        String output = "" + calendar.get(Calendar.HOUR_OF_DAY);
-        return output;
-    }
-
-    private String getTitleFromTime(Calendar start, Calendar end) {
-        int startHour = start.get(Calendar.HOUR_OF_DAY);
-        int endHour = end.get(Calendar.HOUR_OF_DAY);
-        String output = "" + startHour + "-" + endHour;
-        return output;
-    }
-
-
-    public int updateEvent(long eventId, ContentValues contentValues) {
-        ContentResolver cr = mContext.getContentResolver();
-        Uri updateUri;
-        updateUri = ContentUris.withAppendedId(CalendarContract.Events.CONTENT_URI, eventId);
-        int rows = cr.update(updateUri, contentValues, null, null);
-        Log.d(TAG, "updateEvent: Row upated: " + rows);
-        return rows;
-
-    }
-
 
     public List<CalInfo> getAllCalendars() {
-        // Run query
+
         Cursor cur = null;
         ContentResolver cr = mContext.getContentResolver();
         Uri uri = CalendarContract.Calendars.CONTENT_URI;
 
-        String selection = "((" + CalendarContract.Calendars.ACCOUNT_NAME + " = ?) AND ("
+        /*String selection = "((" + CalendarContract.Calendars.ACCOUNT_NAME + " = ?) AND ("
                 + CalendarContract.Calendars.ACCOUNT_TYPE + " = ?) AND ("
                 + CalendarContract.Calendars.OWNER_ACCOUNT + " = ?))";
         String[] selectionArgs = new String[]{"hera@example.com", "com.example",
-                "hera@example.com"};
+                "hera@example.com"};*/
 
-        // Submit the query and get a Cursor object back.
+
         cur = cr.query(uri, CAL_PROJECTION, null, null, null);
 
 
-        // Use the cursor to step through the returned records
 
         List<CalInfo> calInfoList = new ArrayList<>();
         while (cur.moveToNext()) {
@@ -194,50 +124,20 @@ public class CalendarUtil {
         return eventInfoList;
     }
 
-    public List<EventInfo> getEmptyEventsList(long calendarId) {
-        //run query
-        Cursor cur = null;
-
-        String calendarIdString = "" + calendarId;
-        ContentResolver cr = mContext.getContentResolver();
-        Uri uri = CalendarContract.Events.CONTENT_URI;
-
-        String selection = "(" + CalendarContract.Events.CALENDAR_ID + " =?) AND (" +
-                CalendarContract.Events.TITLE + " =?)";
-        String[] selectionArgs = new String[]{
-                calendarIdString,
-                "null"
-        };
-
-        cur = cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
-
-
-        // Use the cursor to step through the returned records
-
-        List<EventInfo> eventInfoList = new ArrayList<>();
-        while (cur.moveToNext()) {
-
-            // Get the field values
-            long id = cur.getLong(cur.getColumnIndex(CalendarContract.Events._ID));
-            String title = cur.getString(cur.getColumnIndex(CalendarContract.Events.TITLE));
-            long start = cur.getLong(cur.getColumnIndex(CalendarContract.Events.DTSTART));
-            long end = cur.getLong(cur.getColumnIndex(CalendarContract.Events.DTEND));
-
-            EventInfo eventInfo = new EventInfo(id, title, start, end);
-
-            eventInfoList.add(eventInfo);
-
-        }
-
-        cur.close();
-        return eventInfoList;
-    }
-
-
     private void updateEventTitle(long eventId, String title) {
         ContentValues contentValues = new ContentValues();
         contentValues.put(CalendarContract.Events.TITLE, title);
         updateEvent(eventId, contentValues);
+    }
+
+    public int updateEvent(long eventId, ContentValues contentValues) {
+        ContentResolver cr = mContext.getContentResolver();
+        Uri updateUri;
+        updateUri = ContentUris.withAppendedId(CalendarContract.Events.CONTENT_URI, eventId);
+        int rows = cr.update(updateUri, contentValues, null, null);
+        Log.d(TAG, "updateEvent: Row upated: " + rows);
+        return rows;
+
     }
 
 
